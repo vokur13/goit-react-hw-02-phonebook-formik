4b63dce53ad0582acca5654f80c327a7b17dc884
vokur13 <vokur13@gmail.com>
2022-10-02 16:35:03 +0300

    fixed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -44,8 +44,6 @@ export const ContactForm = ({ onSubmit }) => {
             id={nameID}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            // value={name}
-            // onChange={this.handleChange}
           />
           <Error component="div" name="name" />
           <Label htmlFor={numberID}>Number</Label>
@@ -55,8 +53,6 @@ export const ContactForm = ({ onSubmit }) => {
             id={numberID}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            //     value={number}
-            //     onChange={this.handleChange}
           />
           <Error component="div" name="number" />
           <Button type="submit">Add contact</Button>
